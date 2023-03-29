79c9d0b9f573e08e9344da12284268288586928b
Sazzad171 <sajjad.cse.diu@gmail.com>
2023-03-29 23:29:07 +0600

    update

--- src/pages/home/banner/Banner.jsx
+++ src/pages/home/banner/Banner.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from 'react';
+
+// bootstrap components
+import {Container, Row, Col} from 'react-bootstrap';
+
+// import imgs
+import BannerLayers from '../../../assets/img/banner-layers.png';
 
 export default function Banner() {
   return (
-    <div>Banner</div>
+    <section className="banner-area">
+      <Container>
+        <Row className='align-items-center'>
+          <Col md={7}>
+            <h1>Discover, and collect Digital Art  NFTs </h1>
+            <p>
+              Digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, Sell, and discover exclusive digital assets.
+            </p>
+            <p>
+              <a href="#f" className="btn btn-blue-lg">Explore Now</a>
+            </p>
+            <div className="d-flex gap-4">
+              <div>
+                <h2>98K+</h2>
+                <p>Artwork</p>
+              </div>
+              <div>
+
+              </div>
+              <div>
+
+              </div>
+            </div>
+          </Col>
+          <Col md={5}>
+            <img src={BannerLayers} alt="banner layers" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
