916d63343fc983db492cf61151f0dd20d4ae49cd
Sazzad171 <sajjad.cse.diu@gmail.com>
2023-03-30 23:14:14 +0600

    update

--- src/pages/home/features/Features.jsx
+++ src/pages/home/features/Features.jsx
@@ -7,24 +7,45 @@ import {Container, Row, Col} from 'react-bootstrap';
 import './features.css';
 
 // import imgs
-
+import CardImg from '../../../assets/img/transaction-icon.png';
+import GrowthImg from '../../../assets/img/growth-icon.png';
 
 export default function Features() {
   return (
-    <section className="banner-area position-relative">
+    <section className="features-area">
       <Container>
-        <Row className='align-items-center'>
+        <Row>
           {/* left col */}
           <Col md={4}>
-            
+            <h4 className="features-heading">The amazing NFT art <br /> of the world here</h4>
           </Col>
           {/* middle col */}
           <Col md={4}>
-            
+            <div className="feature-item d-flex gap-3">
+              <div>
+                <img src={CardImg} alt="img" className="img-fluid" />
+              </div>
+              <div>
+                <h6 className='font-dm-sans font-700'>Fast Transaction</h6>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam etiam viverra tellus imperdiet.
+                </p>
+              </div>
+            </div>
           </Col>
           {/* right col */}
           <Col md={4}>
-            
+            <div className="feature-item d-flex gap-3">
+              <div>
+                <img src={GrowthImg} alt="img" className="img-fluid" />
+              </div>
+              <div>
+                <h6 className='font-dm-sans font-700'>Growth Transaction</h6>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam etiam viverra tellus
+                </p>
+              </div>
+            </div>
           </Col>
         </Row>
       </Container>
--- src/pages/home/top-collections/TopCollections.jsx
+++ src/pages/home/top-collections/TopCollections.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React from 'react';
+
+// import bootstrap components
+import { Col, Container, Row } from 'react-bootstrap';
+
+// import custom css
+import './top-collections.css';
+
+// import imgs
+import MainColc from '../../../assets/img/main-colc.png';
+import OtherCol1 from '../../../assets/img/other-colc-1.png';
+import OtherCol2 from '../../../assets/img/other-colc-2.png';
+import OtherCol3 from '../../../assets/img/other-colc-3.png';
+import user from '../../../assets/img/user.png';
+import Etherium from '../../../assets/img/ethereum-2.svg';
 
 export default function TopCollections() {
   return (
-    <div>TopCollections</div>
+    <section className='top-collection-area'>
+      <Container>
+        <Row>
+          {/* left col */}
+          <Col md={4}>
+            <div className="main-colc-item-box">
+              <img src={MainColc} alt="collection" className="img-fluid" />
+              <div className="d-flex justify-content-between">
+                <div className='d-flex'>
+                  <img src={user} alt="user" className="img-fluid" />
+                  <div>
+                    <h6>The Futr Abstr</h6>
+                    <p>10 in the stock</p>
+                  </div>
+                </div>
+                <div>
+                  <p>Highest Bid</p>
+                  <div className="d-flex">
+                    <img src={Etherium} alt="icon" className="img-fluid" />
+                    <p>0.25 ETH</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          {/* middle col */}
+          <Col md={4}>
+            <div className="other-colc-items-box">
+
+            </div>
+          </Col>
+          {/* right col */}
+          <Col md={4}>
+            <div className="top-colc-box">
+
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
