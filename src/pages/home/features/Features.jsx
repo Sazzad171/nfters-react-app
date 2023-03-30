a30e8e92a7086fcf7f55ee0bbc2048a4e54d4ab6
Sazzad171 <sajjad.cse.diu@gmail.com>
2023-03-30 11:27:22 +0600

    update

--- src/pages/home/features/Features.jsx
+++ src/pages/home/features/Features.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from 'react';
+
+// bootstrap components
+import {Container, Row, Col} from 'react-bootstrap';
+
+// import css
+import './features.css';
+
+// import imgs
+
 
 export default function Features() {
   return (
-    <div>Features</div>
+    <section className="banner-area position-relative">
+      <Container>
+        <Row className='align-items-center'>
+          {/* left col */}
+          <Col md={4}>
+            
+          </Col>
+          {/* middle col */}
+          <Col md={4}>
+            
+          </Col>
+          {/* right col */}
+          <Col md={4}>
+            
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
