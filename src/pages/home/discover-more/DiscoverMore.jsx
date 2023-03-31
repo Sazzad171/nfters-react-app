3e3e1f58a9618eed948e11bf554fb2664e0d55a7
Sazzad171 <sajjad.cse.diu@gmail.com>
2023-03-31 21:38:43 +0600

    update

--- src/pages/home/discover-more/DiscoverMore.jsx
+++ src/pages/home/discover-more/DiscoverMore.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React from 'react';
+
+// import bootstrap components
+import { Col, Container, Row } from 'react-bootstrap';
+
+// import custom css
+import './discover-more.css';
+
+// import imgs
+import MainColc from '../../../assets/img/main-colc.png';
+
+// icons import
+import {FaEthereum} from 'react-icons/fa';
 
 export default function DiscoverMore() {
+
   return (
-    <div>DiscoverMore</div>
+    <section className='discover-more-area'>
+      <Container>
+        <h3 className="section-heading">Discover more NFTs</h3>
+
+        {/* nav tab */}
+        <Row>
+          {/* nav tab col */}
+          <Col md={9}>
+            
+          </Col>
+          {/* filter col */}
+          <Col md={3}>
+            
+          </Col>
+        </Row>
+
+        {/* tab content */}
+        
+        <Row>
+          {/* left col */}
+          <Col md={3}>
+            
+          </Col>
+          {/* middle col */}
+          <Col md={3}>
+            
+          </Col>
+          {/* right col */}
+          <Col md={3}>
+            
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
